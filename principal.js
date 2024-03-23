4f95b7a7914bee5c83bf7e9a98f879af0a6e82df
Bruno Sanzovo <gamebruno@gmail.com>
2024-03-23 10:01:12 -0300

    main

---
+++ principal.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formLogin = document.querySelector('.form-login');
+    const forgotPasswordLink = document.getElementById('forgotPasswordLink');
+
+    formLogin.addEventListener('submit', function (event) {
+        event.preventDefault(); // Evita o envio padrão do formulário
+
+        // Obtenha os valores dos campos de entrada
+        const email = formLogin.querySelector('input[type="text"]').value;
+        const senha = formLogin.querySelector('input[type="password"]').value;
+
+        // Obtenha os dados salvos no localStorage
+        const savedEmail = localStorage.getItem('login');
+        const savedSenha = localStorage.getItem('senha');
+
+        // Verifique se os campos não estão vazios
+        if (email.trim() === '' || senha.trim() === '') {
+            alert('Por favor, preencha todos os campos.');
+            return;
+        }
+
+        // Verifique se os dados inseridos correspondem aos salvos
+        if (email === savedEmail && senha === savedSenha) {
+            // Redirecione para a página inicial (index.html) se o login for bem-sucedido
+            window.location.href = 'index.html';
+        } else {
+            // Exiba uma mensagem de erro caso o login falhe
+            alert('Login ou senha incorretos. Por favor, tente novamente.');
+        }
+    });
+
+    const entreAquiButton = document.querySelector('.button-login');
+
+    entreAquiButton.addEventListener('click', function (event) {
+        // Impede o redirecionamento padrão ao clicar no botão
+        event.preventDefault();
+
+        // Dispara o evento de submit no formulário de login
+        formLogin.dispatchEvent(new Event('submit'));
+    });
+
+    // Adiciona um event listener para o clique no link "Esqueci a senha"
+    forgotPasswordLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Você esqueceu sua senha! Entre em contato com o suporte para recuperá-la.');
+        // Aqui você pode adicionar lógica adicional, como abrir um modal de recuperação de senha.
+    });
+
+    // Mensagem de manutenção para os botões Google e Facebook
+    const googleButton = document.querySelector('.google-button');
+    const facebookButton = document.querySelector('.facebook-button');
+
+    googleButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        alert('Entrar pela Google está em manutenção. Desculpe pelo transtorno. Obrigado pela compreensão.');
+    });
+
+    facebookButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        alert('Entrar pelo Facebook está em manutenção. Desculpe pelo transtorno. Obrigado pela compreensão.');
+    });
+});
---
+++ script.js
@@ -0,0 +1,142 @@
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+const completedExercisesList = document.getElementById('completed-exercises-list');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const resetButton = document.getElementById('reset');
+const histButton = document.getElementById('history');
+
+let timer;
+let isRunning = false;
+let remainingTime = 25 * 60; // Inicialmente definido como 25 minutos
+let stretchInterval;
+let completedExercises = [];
+
+const stretchExercises = [
+    "Levante-se e estique os braços acima da cabeça por 10 segundos.",
+    "Incline o tronco para os lados, mantendo o alongamento por 15 segundos em cada lado.",
+    "Gire os ombros para trás em movimentos circulares por 20 segundos.",
+    "Faça uma rotação no pescoço, movendo-o suavemente para a esquerda e para a direita por 10 segundos.",
+    "Estique as pernas e toque os dedos dos pés, mantendo a posição por 15 segundos.",
+    
+];
+
+// Recupera os dados do localStorage, se houver
+let storedRemainingTime = localStorage.getItem('remainingTime');
+let storedCompletedExercises = localStorage.getItem('completedExercises');
+
+// Verifica se há dados salvos e atribui-os às variáveis correspondentes
+remainingTime = storedRemainingTime ? parseInt(storedRemainingTime) : 25 * 60;
+completedExercises = storedCompletedExercises ? JSON.parse(storedCompletedExercises) : [];
+
+// Atualiza a exibição do temporizador e dos exercícios completados
+updateTimerDisplay();
+updateCompletedExercisesList();
+
+function updateTimerDisplay() {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+    minutesDisplay.textContent = minutes < 10 ? `0${minutes}` : minutes;
+    secondsDisplay.textContent = seconds < 10 ? `0${seconds}` : seconds;
+}
+
+function updateCompletedExercisesList() {
+    completedExercisesList.innerHTML = ''; // Limpa a lista antes de adicionar os exercícios
+
+    completedExercises.forEach(exercise => {
+        addToCompletedExercises(exercise);
+    });
+}
+
+function startTimer() {
+    if (!isRunning) {
+        timer = setInterval(updateTimer, 1000);
+        isRunning = true;
+        showExercise();
+    }
+}
+function startTimer2() {
+    if (!isRunning) {
+        isRunning = false;
+        showExercise2();
+    }
+}
+function pauseTimer() {
+    clearInterval(timer);
+    clearInterval(stretchInterval);
+    isRunning = false;
+
+    // Salva os dados no localStorage ao pausar o temporizador
+    localStorage.setItem('remainingTime', remainingTime);
+    localStorage.setItem('completedExercises', JSON.stringify(completedExercises));
+}
+
+function updateTimer() {
+    remainingTime--;
+    if (remainingTime >= 0) {
+        updateTimerDisplay();
+    } else {
+        clearInterval(timer);
+        completeExerciseButton.style.display = 'block';
+    }
+}
+
+function showExercise() {
+    let index = 0;
+    stretchInterval = setInterval(() => {
+        const currentExercise = stretchExercises[index];
+        addToCompletedExercises(currentExercise);
+        index = (index + 1) % stretchExercises.length;
+    }, 5 * 60 * 1000); // 5 minutos
+}
+function showExercise2(exercise) {
+    alert("1ªLevante-se e estique os braços acima da cabeça por 10 segundos\n2ªIncline o tronco para os lados, mantendo o alongamento por 15 segundos em cada lado.\n3ªGire os ombros para trás em movimentos circulares por 20 segundos.\n4ªFaça uma rotação no pescoço, movendo-o suavemente para a esquerda e para a direita por 10 segundos.\n5ªEstique as pernas e toque os dedos dos pés, mantendo a posição por 15 segundos.");
+}
+function completeExercise() {
+    const currentExercise = stretchExercises.find(exercise => exercise === exerciseDescription.textContent.replace('Exercício de Alongamento: ', ''));
+    if (currentExercise && !completedExercises.includes(currentExercise)) {
+        completedExercises.push(currentExercise);
+        addToCompletedExercises(currentExercise);
+    }
+    completeExerciseButton.style.display = 'none';
+
+    // Salva os dados no localStorage ao completar um exercício
+    localStorage.setItem('completedExercises', JSON.stringify(completedExercises));
+}
+
+function addToCompletedExercises(exercise) {
+    const li = document.createElement('li');
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+    const timeElapsed = `${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    li.textContent = `${exercise} - Tempo: ${timeElapsed}`;
+    const img = document.createElement('img');
+    img.src = 'assets/tarefaicon01.jpeg';
+    img.alt = 'checkmark';
+    img.className = 'task-img';
+    li.appendChild(img);
+    completedExercisesList.appendChild(li);
+
+    // Adiciona classe automaticamente para destacar exercícios concluídos após 20 segundos
+    setTimeout(() => {
+        li.classList.add('completed');
+    }, 60000);
+}
+
+startButton.addEventListener('click', startTimer);
+pauseButton.addEventListener('click', pauseTimer);
+
+resetButton.addEventListener('click', resetTimer);
+histButton.addEventListener('click', startTimer2);
+
+function resetTimer() {
+    clearInterval(timer); // Limpa o temporizador
+    clearInterval(stretchInterval); // Limpa o intervalo de alongamento
+    remainingTime = 25 * 60; // Reinicia o tempo restante para o valor inicial
+    isRunning = false; // Define o estado de execução como falso
+    completedExercises = []; // Limpa a lista de exercícios concluídos
+    localStorage.removeItem('remainingTime'); // Remove os dados do localStorage
+    localStorage.removeItem('completedExercises'); // Remove os dados do localStorage
+    updateTimerDisplay(); // Atualiza a exibição do temporizador
+    updateCompletedExercisesList(); // Atualiza a lista de exercícios concluídos
+}
